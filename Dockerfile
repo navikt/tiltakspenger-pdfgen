a479ec95c6ea88b5e60613db2662513109a2465f
Benedicte Myrvoll <benemyr17@gmail.com>
2024-12-18 20:30:40 +0100

    Legg til stansbrev og refaktorer html + css slik at det samsvarer med aksel sine retningslinjer for brev

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY templates /app/templates
 COPY fonts /app/fonts
 COPY resources /app/resources
 
-ENV ENABLE_HTML_ENDPOINT=true
+ENV ENABLE_HTML_ENDPOINT=false
--- run_development.sh
+++ run_development.sh
@@ -8,9 +8,13 @@ docker build -t "$CONTAINER_NAME" "$CURRENT_PATH"
 
 docker run \
         --name "$CONTAINER_NAME" \
-        -v "$CURRENT_PATH"/data:/app/data \
-        -p 8081:8080 \
+        -v $CURRENT_PATH/templates:/app/templates \
+        -v $CURRENT_PATH/fonts:/app/fonts \
+        -v $CURRENT_PATH/data:/app/data \
+        -v $CURRENT_PATH/resources:/app/resources \
+        -p 8085:8080 \
         -e DISABLE_PDF_GET=false \
+        -e JDK_JAVA_OPTIONS \
         -it \
         --rm \
         "$CONTAINER_NAME"
